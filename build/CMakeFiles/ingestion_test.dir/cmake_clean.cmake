9670cb2673ea9dbc2909c61e33f7098eb78065cb
mahida <mahidanayyer07@gmail.com>
2026-07-24 20:48:54 +0500

    Week 5: DICOM ingestion parser

---
+++ build/CMakeFiles/ingestion_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ingestion_test.dir/src/ingestion/main.cpp.o"
+  "CMakeFiles/ingestion_test.dir/src/ingestion/main.cpp.o.d"
+  "ingestion_test"
+  "ingestion_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ingestion_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
